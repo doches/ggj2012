8d174914986db5af17850f7fa727be5f93ec3d27
Andy Durdin <me@andy.durdin.net>
2012-01-28 14:55:25 +0000

    Bullets no longer kill things.

--- GGJUnityProject/Assets/Scripts/playerBulletCollisions.cs
+++ GGJUnityProject/Assets/Scripts/playerBulletCollisions.cs
@@ -3,16 +3,4 @@ using System.Collections;
 
 public class playerBulletCollisions : MonoBehaviour 
 {
-	void OnTriggerEnter(Collider other) 
-	{
-		
-		print(other.gameObject.name);
-		if (other.gameObject.name.Equals("SpazBullet(Clone)") == false)
-		{
-			Destroy(other.gameObject);
-			Destroy(gameObject);
-		}
-		
-		
-	}
 }
