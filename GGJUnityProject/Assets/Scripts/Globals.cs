0dcd2033145f9ecc516c2e96a3e0cd2ad64dcd8f
unknown <Administrator@seriousMachine.(none)>
2012-01-28 02:56:03 +0100

    made things for player movement, basics of the GUI, bounding box for player movement, and something

---
+++ GGJUnityProject/Assets/Scripts/Globals.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public static class Globals {
+
+    public static int PlayerLife = 10;
+
+    
+
+	// Use this for initialization
+    //static void Start () {
+    //    PlayerLife 
+    //}
+	
+    //// Update is called once per frame
+    //void Update () {
+	
+    //}
+
+    
+
+}
