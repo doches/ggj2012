c745b9acc28a1e4264e74bade262f1aa37f51a5c
Andy Durdin <me@andy.durdin.net>
2012-01-29 02:08:26 +0000

    Reorganised weapon system; added wimpy (zeroth) weapon, gatling (first) weapon, cannonball (second) weapon.

---
+++ GGJUnityProject/Assets/Scripts/CannonballBullet.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+public class CannonballBullet : MonoBehaviour 
+{
+	public static bool CannonballBulletWentUp = false;
+	public float magnitude = 1.0f;
+	public float frequency = 2.0f;
+	public float speed = 200.0f;
+
+	private Vector3 startPosition;
+	private float offset;
+
+	void Start() {
+		startPosition = transform.position;
+		if (CannonballBulletWentUp) {
+			offset = 3.14159f;
+			CannonballBulletWentUp = false;
+		} else {
+			offset = 0;
+			CannonballBulletWentUp = true;
+		}
+	}
+	
+	void Update() {
+		transform.position = new Vector3(
+			transform.position.x + speed * Time.deltaTime,
+			magnitude * Mathf.Sin(offset + frequency * (transform.position.x - startPosition.x) * 3.1415f / 180f),
+			startPosition.z
+		);
+	}
+}
