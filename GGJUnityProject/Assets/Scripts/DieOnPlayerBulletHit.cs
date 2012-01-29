edd14405f1492206192c7aa64192f5b9441cc677
Trevor Fountain <doches@gmail.com>
2012-01-29 13:41:57 +0000

    Adde dDavid's sound prefabliink

--- GGJUnityProject/Assets/Scripts/DieOnPlayerBulletHit.cs
+++ GGJUnityProject/Assets/Scripts/DieOnPlayerBulletHit.cs
@@ -1,13 +1,6 @@
 using UnityEngine;
 using System.Collections;
 
-<<<<<<< HEAD
-
-public class DieOnPlayerBulletHit : MonoBehaviour {
-	public GameObject ExplosionPrefab;
-	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.layer == 9) { // player bullets
-=======
 public class DieOnPlayerBulletHit : MonoBehaviour 
 {
 	public GameObject ExplosionPrefab;
@@ -17,8 +10,8 @@ public class DieOnPlayerBulletHit : MonoBehaviour
 	{
 		print (this.gameObject.name);
 		if (other.gameObject.layer == 9) 
-		{ // player bullets
->>>>>>> 7d5b4b840adf73cfe27ada8fce1c5752f017d39b
+		{ 
+			// player bullets
 			// Die
 			Destroy(this.gameObject);
 			// Also destroy the player's bullet
