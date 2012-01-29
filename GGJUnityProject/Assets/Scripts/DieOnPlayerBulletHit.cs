3f7678dc6243a33f952389b9c20ed0dff3107c2a
unknown <dave@dave-PC.(none)>
2012-01-29 07:14:24 +0000

    'Splosions, man. Explosions.

--- GGJUnityProject/Assets/Scripts/DieOnPlayerBulletHit.cs
+++ GGJUnityProject/Assets/Scripts/DieOnPlayerBulletHit.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 using System.Collections;
 
-public class DieOnPlayerBulletHit : MonoBehaviour {
-	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.name.Equals("Bullet(Clone)")) {
+public class DieOnPlayerBulletHit : MonoBehaviour 
+{
+	public GameObject ExplosionPrefab;
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.name.Equals("Bullet(Clone)")) 
+		{
 			// Die
 			Destroy(this.gameObject);
 			// Also destroy the player's bullet
 			Destroy(other.gameObject);
+			Instantiate(ExplosionPrefab, transform.position, Quaternion.identity);
 		}
 	}
 }
